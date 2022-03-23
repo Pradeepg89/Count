c68877ba63d7f39bd45e71470a8fda328b6ae3bb
Pradeep Pandey <pradeeppandey@Pradeeps-MacBook-Air.local>
2022-03-23 13:39:01 +0530

    inital add

---
+++ windows/Count/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Count::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Count::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
